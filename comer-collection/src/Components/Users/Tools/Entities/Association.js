5a0ad173acdb6e82093f9ca5971b5482182e538f
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-04-08 15:34:57 -0500

    Create Association class

---
+++ comer-collection/src/Components/Users/Tools/Entities/Association.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+
+class Association {
+
+    static baseUrl = null;
+    static primary = null;
+    static secondary = null;
+
+    static assignPresent = "assign";
+    static assignPast = "assigned";
+    static unassignPresent = "unassign";
+    static unassignPast = "unassigned";
+
+
+}
+
+export { Association };
