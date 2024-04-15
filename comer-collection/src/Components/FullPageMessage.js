7f90454a37c32cccb254113b8033db4a24dde1b0
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-04-15 15:39:39 -0500

    Update FullPageMessage.js
    Run ESLint

--- comer-collection/src/Components/FullPageMessage.js
+++ comer-collection/src/Components/FullPageMessage.js
@@ -4,13 +4,12 @@ import { useNavigate } from "react-router";
 import PropTypes from "prop-types";
 
 export const FullPageMessage = ({ message, buttonText, buttonDestination, Icon, buttonAction }) => {
-
     const navigate = useNavigate();
 
     return (
-        <Box component={Paper} square sx={{width: "100%", height: "100%", boxSizing: "border-box", padding: "50px"}}>
-            <Stack direction="column" alignItems="center" justifyContent="center" spacing={2} sx={{height: "100%"}}>
-                <Icon sx={{fontSize: "150pt", opacity: 0.5}} />
+        <Box component={Paper} square sx={{ width: "100%", height: "100%", boxSizing: "border-box", padding: "50px" }}>
+            <Stack direction="column" alignItems="center" justifyContent="center" spacing={2} sx={{ height: "100%" }}>
+                <Icon sx={{ fontSize: "150pt", opacity: 0.5 }} />
                 <Typography variant="h4">{message}</Typography>
                 {(buttonDestination || buttonText) && (
                     <Button variant="contained" onClick={buttonAction ?? (() => navigate(buttonDestination ?? "/SignIn"))}>
