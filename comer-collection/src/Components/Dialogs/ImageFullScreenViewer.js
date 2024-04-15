ade2b8bc09279908f636881ac6cdc63e90087942
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-04-15 15:04:21 -0500

    Update ImageFullScreenViewer.js
    Run ESLint

--- comer-collection/src/Components/Dialogs/ImageFullScreenViewer.js
+++ comer-collection/src/Components/Dialogs/ImageFullScreenViewer.js
@@ -50,18 +50,17 @@ export const ImageFullScreenViewer = ({ image, setImage, previewerOpen, setPrevi
             displayName: "Location"
         }];
 
-
     const artists = image?.Artists.sort((a, b) => {
         return a.fullNameReverse < b.fullNameReverse ? 1 : -1;
     });
-    
-    
+
     return image && (
-        <Dialog open={previewerOpen}  maxWidth="lg"  sx={{zIndex: 5000}}>
-            
+        <Dialog open={previewerOpen} maxWidth="lg" sx={{ zIndex: 5000 }}>
+
             <DialogTitle textAlign="center" variant="h4">{image.title}</DialogTitle>
 
-            <DialogContent sx={{height: "100%",
+            <DialogContent sx={{
+                height: "100%",
                 display: "grid",
                 gridTemplateColumns: "20px 60% 30px 30% 20px",
                 gridTemplateRows: "1fr",
@@ -71,34 +70,34 @@ export const ImageFullScreenViewer = ({ image, setImage, previewerOpen, setPrevi
                 overflow: "hidden"
             }}>
                 <Stack gridArea="image" maxHeight="500px" alignContent="center">
-                    <img src={`${process.env.REACT_APP_API_HOST}/api/public/images/${image.id}/download`} 
-                        style={{objectFit: "contain"}} width="auto" height="100%"
+                    <img src={`${process.env.REACT_APP_API_HOST}/api/public/images/${image.id}/download`}
+                        style={{ objectFit: "contain" }} width="auto" height="100%"
                     />
                 </Stack>
                 <Stack gridArea="fields" overflow="auto" spacing={2}>
                     {/* Artists */}
                     <Stack direction="column">
                         <Typography variant="h6">{
-                            artists.length <= 1 && "Artist" || 
-                            artists.length > 1 && "Artists"
+                            (artists.length <= 1 && "Artist") ||
+                            (artists.length > 1 && "Artists")
                         }</Typography>
-                        {artists.length >= 1 && artists.map((a) => (
+                        {(artists.length >= 1 && artists.map((a) => (
                             <Stack direction="row" alignItems="center" key={a.id}>
                                 <PersonIcon />
                                 <Typography variant="body1">{a.safe_display_name}</Typography>
                             </Stack>
-                        )) || artists.length == 0 && (
+                        ))) || (artists.length === 0 && (
                             <Stack direction="row" alignItems="center">
                                 <PersonIcon opacity="0.5" />
-                                <Typography sx={{opacity: 0.5}} variant="body1">No artist listed</Typography>
+                                <Typography sx={{ opacity: 0.5 }} variant="body1">No artist listed</Typography>
                             </Stack>
-                        )}
+                        ))}
                     </Stack>
                     <Stack direction="column" alignItems="left" spacing={2} height="100%">
                         {fields.map((field) => image[field.fieldName] && (
                             <Stack direction="column" key={field.fieldName}>
                                 <Typography variant="h6">{field.displayName}</Typography>
-                                <Typography sx={{opacity: 0.5}} variant="body1">{
+                                <Typography sx={{ opacity: 0.5 }} variant="body1">{
                                     image[field.fieldName]
                                 }</Typography>
                             </Stack>
