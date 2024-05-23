03cd2da9bedf26d39ac10836d030147b7587fda1
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-05-23 17:55:12 -0500

    Optimize SearchBox with styled

--- comer-collection/src/Components/SearchBox.js
+++ comer-collection/src/Components/SearchBox.js
@@ -1,42 +1,57 @@
-import React from "react";
-import { TextField, InputAdornment, IconButton } from "@mui/material";
+/* eslint-disable react/no-multi-comp */
+import React, { useCallback, useMemo } from "react";
+import { TextField, InputAdornment, IconButton, styled } from "@mui/material";
 import { SearchIcon, ClearIcon } from "../Imports/Icons.js";
 import PropTypes from "prop-types";
 
+/**
+ * Use as regular IconButton and add isVisible Boolean property
+ */
+const DisappearingIconButton = styled(IconButton)(({ isVisible }) => ({
+    display: isVisible ? "" : "none"
+}));
+
+const FixedWidthTextField = styled(TextField)(({ width }) => ({
+    width
+}));
+
 const SearchBox = ({ searchQuery, setSearchQuery, width, placeholder }) => {
+    const handleEditSearchQuery = useCallback((e) => {
+        setSearchQuery(e.target.value);
+    }, [setSearchQuery]);
+
+    const handleClearSearchQuery = useCallback(() => {
+        setSearchQuery("");
+    }, [setSearchQuery]);
+
+    const textfieldInputProps = useMemo(() => ({
+        height: "100%",
+        startAdornment: (
+            <InputAdornment position="start">
+                <SearchIcon />
+            </InputAdornment>
+        ),
+        endAdornment: (
+            <InputAdornment position="end">
+                <DisappearingIconButton
+                    isVisible={Boolean(searchQuery)}
+                    onClick={handleClearSearchQuery}
+                >
+                    <ClearIcon />
+                </DisappearingIconButton>
+            </InputAdornment>
+        )
+
+    }), [handleClearSearchQuery, searchQuery]);
+
     return (
-        <TextField
-            InputProps={{
-                sx: {
-                    height: "100%"
-                },
-                startAdornment: (
-                    <InputAdornment position="start">
-                        <SearchIcon />
-                    </InputAdornment>
-                ),
-                endAdornment: (
-                    <InputAdornment position="end">
-                        <IconButton
-                            onClick={() => {
-                                setSearchQuery("");
-                            }}
-                            sx={{
-                                display: searchQuery === "" ? "none" : ""
-                            }}
-                        >
-                            <ClearIcon />
-                        </IconButton>
-                    </InputAdornment>
-                )
-            }}
-            onChange={(e) => {
-                setSearchQuery(e.target.value);
-            }}
+        <FixedWidthTextField
+            InputProps={textfieldInputProps}
+            onChange={handleEditSearchQuery}
             placeholder={placeholder ?? "Search"}
-            sx={{ width }}
             value={searchQuery}
             variant="outlined"
+            width={width}
         />
     );
 };
