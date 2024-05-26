1e6dd85c2875de0bed3a1190c870f5c05c93c8a3
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-05-26 18:42:01 -0500

    Use InView with CollectionBrowser

--- comer-collection/src/Pages/Browsers/CollectionBrowser.js
+++ comer-collection/src/Pages/Browsers/CollectionBrowser.js
@@ -4,12 +4,16 @@ import { SellIcon, PersonIcon } from "../../Imports/Icons.js";
 import { ThumbnailBox } from "../../Components/CollectionBrowser/ThumbnailBox.js";
 import { useLoaderData } from "react-router";
 import { CollectionGalleryDisplay } from "../../Components/CollectionGalleryDisplay.js";
+import { useInView } from "react-intersection-observer";
 
 export const CollectionBrowserImageContainer = ({ image, viewMode, isSelected, setSelectedItem, isDisabled }) => {
+    const { inView, ref } = useInView();
+
     const infoStack = useMemo(() => (
         <Stack
             direction={viewMode === "list" ? "row" : "column"}
             padding={4}
+            ref={ref}
             spacing={2}
             sx={{
                 width: viewMode === "list" ? "500px" : "200px"
@@ -17,64 +21,69 @@ export const CollectionBrowserImageContainer = ({ image, viewMode, isSelected, s
         >
             <ThumbnailBox image={image} />
 
-            <Stack
-                alignItems={viewMode === "list" ? "left" : "center"}
-                direction="column"
-                spacing={1}
-            >
-                <Typography variant="h6">
-                    {image.title}
-                </Typography>
+            {inView
+                ? (
+                    <Stack
+                        alignItems={viewMode === "list" ? "left" : "center"}
+                        direction="column"
+                        spacing={1}
+                    >
+                        <Typography variant="h6">
+                            {image.title}
+                        </Typography>
 
-                {viewMode === "list" && (
-                    <Typography variant="body1">
-                        {image.year}
-                    </Typography>
-                )}
-
-                <Stack
-                    direction={viewMode === "list" ? "column" : "row"}
-                    spacing={viewMode === "list" ? 0 : 2}
-                >
-                    {image.Artists.map((a) => (
-                        <Stack
-                            alignItems="center"
-                            direction="row"
-                            key={a.id}
-                            spacing={1}
-                        >
-                            <PersonIcon />
-
+                        {viewMode === "list" && (
                             <Typography variant="body1">
-                                {a.fullName}
+                                {image.year}
                             </Typography>
-                        </Stack>
-                    ))}
-                </Stack>
+                        )}
 
-                <Stack
-                    direction="row"
-                    flexWrap="wrap"
-                    spacing={1}
-                    useFlexGap
-                >
-                    {viewMode === "list" && image.Tags.map((t) => (
-                        <Chip
-                            icon={<SellIcon />}
-                            key={t.id}
-                            label={
-                                <Typography>
-                                    {t.data}
-                                </Typography>
-                            }
-                            sx={{ maxWidth: "150px" }}
-                            variant="filled"
-                        />
-                    ))}
-                </Stack>
-            </Stack>
+                        <Stack
+                            direction={viewMode === "list" ? "column" : "row"}
+                            spacing={viewMode === "list" ? 0 : 2}
+                        >
+                            {image.Artists.map((a) => (
+                                <Stack
+                                    alignItems="center"
+                                    direction="row"
+                                    key={a.id}
+                                    spacing={1}
+                                >
+                                    <PersonIcon />
+
+                                    <Typography variant="body1">
+                                        {a.fullName}
+                                    </Typography>
+                                </Stack>
+                            ))}
+                        </Stack>
+
+                        <Stack
+                            direction="row"
+                            flexWrap="wrap"
+                            spacing={1}
+                            useFlexGap
+                        >
+                            {viewMode === "list" && image.Tags.map((t) => (
+                                <Chip
+                                    icon={<SellIcon />}
+                                    key={t.id}
+                                    label={
+                                        <Typography>
+                                            {t.data}
+                                        </Typography>
+                                    }
+                                    sx={{ maxWidth: "150px" }}
+                                    variant="filled"
+                                />
+                            ))}
+                        </Stack>
+                    </Stack>
+                )
+                : null}
+
         </Stack>
-    ), [image, viewMode]);
+    ), [image, inView, ref, viewMode]);
 
     const listItemButton = useMemo(() => (
         <ListItemButton
