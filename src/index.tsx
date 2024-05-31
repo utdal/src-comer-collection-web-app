ea4c84e34a8b3d3bab59daa15c217ce23ddcfe17
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-05-31 09:26:00 -0500

    Fix index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import { createRoot } from "react-dom/client";
+import type { Container } from "react-dom/client";
 
-const container = document.getElementById("root");
+const container = document.getElementById("root") as Container;
 const root = createRoot(container);
 root.render(
     <React.StrictMode>
