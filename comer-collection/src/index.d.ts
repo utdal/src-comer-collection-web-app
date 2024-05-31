2f0f23012b021e2a1f4a595f6c74ff0eb6a7881c
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-05-30 23:22:49 -0500

    Update index.d.ts

--- comer-collection/src/index.d.ts
+++ comer-collection/src/index.d.ts
@@ -1,5 +1,6 @@
 // This is not a TypeScript project (at least not yet).
-// I am defining types here only to help with autocomplete
+// I am defining types here only to add them to JSDocs
+// throughout the project, and also to help with autocomplete
 // in Visual Studio Code.
 
 type Item = {
