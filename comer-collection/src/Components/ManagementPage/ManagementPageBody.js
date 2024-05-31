d4d67f85506ec3d8194c9445d9fcad14fc395c62
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-05-30 20:04:14 -0500

    Remove old isLoaded and isError logic from ManagementPageBody

--- comer-collection/src/Components/ManagementPage/ManagementPageBody.js
+++ comer-collection/src/Components/ManagementPage/ManagementPageBody.js
@@ -1,9 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Box, styled } from "@mui/material";
-import { useEntity, useItemsLoadStatus, useManagementCallbacks } from "../../ContextProviders/ManagementPageProvider.js";
-import { FullPageMessage } from "../FullPageMessage.js";
-import { WarningIcon } from "../../Imports/Icons.js";
 
 const PositionedBox = styled(Box)(() => ({
     gridArea: "table",
@@ -11,31 +8,9 @@ const PositionedBox = styled(Box)(() => ({
 }));
 
 export const ManagementPageBody = ({ children }) => {
-    const [isLoaded, isError] = useItemsLoadStatus();
-    const { handleRefresh } = useManagementCallbacks();
-    const Entity = useEntity();
-
     return (
         <PositionedBox>
-            {isError
-                ? (
-                    <FullPageMessage
-                        Icon={WarningIcon}
-                        buttonAction={handleRefresh}
-                        buttonText="Retry"
-                        message={`Error loading ${Entity.plural}`}
-                    />
-                )
-                : !isLoaded
-                    ? (
-                        <FullPageMessage
-                            includeLinearProgress
-                            message={`Loading ${Entity.plural}...`}
-                        />
-                    )
-                    : (
-                        children
-                    )}
+            {children}
         </PositionedBox>
     );
 };
