b02fe3c1e8ac16bae60533cbaec3bd33bf556d9c
vmm20 <57491580+vmm20@users.noreply.github.com>
2024-05-31 01:10:47 -0500

    Update index.d.ts

--- comer-collection/src/index.d.ts
+++ comer-collection/src/index.d.ts
@@ -1,7 +1,9 @@
 // This is not a TypeScript project (at least not yet).
 // I am defining types here only to add them to JSDocs
 // throughout the project, and also to help with autocomplete
-// in Visual Studio Code.
+// in Visual Studio Code.  While editing the project in VS Code,
+// this file should be kept open in the background to allow the 
+// editor to resolve the types.
 
 type Item = {
     id: number,
